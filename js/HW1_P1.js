e8aad9a891ab0cf3ee2509fea96a3cf2d87014a7
Kun Chen <kchen@csie.org>
2020-10-05 05:30:08 +0800

    函式 creatTable 增加 x,y 參數

--- js/HW1_P1.js
+++ js/HW1_P1.js
@@ -1,18 +1,18 @@
 var X = 9
 var Y = 9
 
-var creatTable = (id, callbackfn) => {
+var creatTable = (id, x, y, callbackfn) => {
     $(`table#${id}>thead>tr`).empty();
     $(`table#${id}>thead>tr`).append($('<th>'))
-    for (let i = 0; i <= X; i++) {
+    for (let i = 0; i <= x; i++) {
         $th = $('<th>').text(i)
         $(`table#${id}>thead>tr`).append($th)
     }
     $tbody = $(`table#${id}>tbody`).empty()
-    for (let i = 0; i <= Y; i++) {
+    for (let i = 0; i <= y; i++) {
         $th = $('<th>').text(i)
         $tr = $('<tr>').append($th)
-        for (let j = 0; j <= X; j++) {
+        for (let j = 0; j <= x; j++) {
             $td = callbackfn(j, i)
             $tr.append($td)
         }
@@ -21,7 +21,7 @@ var creatTable = (id, callbackfn) => {
 }
 
 var updateProfit = (A) => {
-    creatTable('profit', (x, y) => {
+    creatTable('profit', X, Y, (x, y) => {
         return $('<td>').text(2 * A.x * x + 2 * A.y * y)
     })
     $(`#profit>tbody>tr:eq(${A.y})>td:eq(${A.x})`).css('background-color', 'rgba(0,123,255,.5)')
@@ -30,14 +30,14 @@ var updateProfit = (A) => {
 var updtaeNet = () => {
     let x1 = +$('#inputX').find(":selected").val()
     let y1 = +$('#inputY').find(":selected").val()
-    creatTable('net', (x2, y2) => {
+    creatTable('net', X, Y, (x2, y2) => {
         return $('<td>').text(2 * x1 * x2 + 2 * y1 * y2 - (x1 ** 2 + y1 ** 2) - (x2 ** 2 + y2 ** 2))
     })
     $(`#net>tbody>tr:eq(${y1})>td:eq(${x1})`).css('background-color', 'rgba(0,123,255,.5)')
 }
 
 $(() => {
-    creatTable('cost', (x, y) => {
+    creatTable('cost', X, Y, (x, y) => {
         return $('<td>').text(x ** 2 + y ** 2)
     })
     let AList1 = Array.apply(null, {
